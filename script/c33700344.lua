ee69627fd4405966abc720eaadef6105eae8d58b
purerosefallen <78877@qq.com>
2018-07-24 13:52:38 +0800

    first

---
+++ script/c33700344.lua
@@ -0,0 +1,7 @@
+--虚拟YouTuber Mirai Akari
+local m=33700344
+local cm=_G["c"..m]
+xpcall(function() require("expansions/script/c37564765") end,function() require("script/c37564765") end)
+function cm.initial_effect(c)
+	
+end
